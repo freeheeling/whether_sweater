3512f3f4811b64834e6a74c3f6a96b5313e7b173
Scott Regenthal <scottregenthal@gmail.com>
2020-03-01 21:18:32 -0700

    Remove hash, initialize methods, create attr_readers for instance vars

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -1,66 +1,31 @@
 class CurrentWeather
+  attr_reader :time_and_date,
+              :weather_conditions,
+              :wether_icon,
+              :temp_F,
+              :feels_like_F,
+              :humidity_percent,
+              :visibility_miles,
+              :uv_index_number,
+              :today_summary,
+              :tonight_summary
+
   def initialize(current_weather_data)
-    @forecast_data = current_weather_data
+    @time_and_date = current_datetime(current_weather_data[:currently][:time])
+    @weather_conditions = current_weather_data[:currently][:summary]
+    @weather_icon = current_weather_data[:currently][:icon]
+    @temp_F = current_weather_data[:currently][:temperature].round
+    @feels_like_F = current_weather_data[:currently][:apparentTemperature].round
+    @humidity_percent = (current_weather_data[:currently][:humidity] * 100).to_i
+    @visibility_miles = current_weather_data[:currently][:visibility]
+    @uv_index_number = current_weather_data[:currently][:uvIndex]
+    @today_summary = current_weather_data[:daily][:summary]
+    @tonight_summary = current_weather_data[:hourly][:data].find do |hour|
+                         hour[:icon].include?('night')
+                       end[:summary]
   end
 
-  def current_conditions
-    {
-      time_and_date: current_datetime,
-      weather_conditions: current_summary,
-      weather_icon: current_icon,
-      temp_F: current_temp,
-      feels_like_F: feels_like,
-      humidity_percent: humidity,
-      visibility_miles: visibility,
-      uv_index_number: uv_index,
-      today_summary: today_summary,
-      tonight_summary: next_night_hour_summary
-    }
-  end
-
-  private
-
-  attr_reader :forecast_data
-
-  def current_datetime
-    Time.at(forecast_data[:currently][:time]).strftime('%-I:%M %p, %-m/%-d')
-  end
-
-  def current_summary
-    forecast_data[:currently][:summary]
-  end
-
-  def current_icon
-    forecast_data[:currently][:icon]
-  end
-
-  def current_temp
-    forecast_data[:currently][:temperature].round
-  end
-
-  def feels_like
-    forecast_data[:currently][:apparentTemperature].round
-  end
-
-  def humidity
-    (forecast_data[:currently][:humidity] * 100).to_i
-  end
-
-  def visibility
-    forecast_data[:currently][:visibility]
-  end
-
-  def uv_index
-    forecast_data[:currently][:uvIndex]
-  end
-
-  def today_summary
-    forecast_data[:daily][:summary]
-  end
-
-  def next_night_hour_summary
-    forecast_data[:hourly][:data].find do |hour|
-      hour[:icon].include?('night')
-    end[:summary]
+  def current_datetime(current_weather_data)
+    Time.at(current_weather_data).strftime('%-I:%M %p, %-m/%-d')
   end
 end
