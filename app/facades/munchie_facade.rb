59f8b1c88afa409d40f7b5926e946061c86e5ee5
Scott Regenthal <scottregenthal@gmail.com>
2020-03-02 10:33:39 -0700

    Find forecast for destination

--- app/facades/munchie_facade.rb
+++ app/facades/munchie_facade.rb
@@ -1,27 +1,29 @@
 class MunchieFacade
-  attr_reader :id
+  attr_reader :id,
+              :end_location,
+              :travel_time
 
   def initialize(origin, destination, cuisine)
     @id = nil
     @origin = origin
     @destination = destination
     @cuisine = cuisine
+    @end_location = route_data.first[:end_address]
+    @travel_time = route_data.first[:duration][:text]
   end
 
   def route_data
     @route_data ||= GoogleService.new(origin, destination).directions_data
   end
 
-  def end_location
-    route_data.first[:end_address]
-  end
-
-  def travel_time
-    route_data.first[:duration][:text]
+  def forecast_data
+    lat = route_data.first[:end_location][:lat] # destination
+    long = route_data.first[:end_location][:lng] # destination
+    @forecast_data ||= DarkskyService.new(lat, long).darksky_data
   end
 
   def forecast
-
+    Forecast.new(forecast_data)
   end
 
   def restaurant
