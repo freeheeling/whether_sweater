7d9345861ac15eee2f78b2b7248cb414546808b1
Scott Regenthal <scottregenthal@gmail.com>
2020-03-01 17:16:11 -0700

    Add test for google geocode service geolocation data instance method

---
+++ spec/services/geocode_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe GeocodeService do
+  context 'instance methods' do
+    context '#geocode_data' do
+      it 'returns geolocation data' do
+        search = GeocodeService.new('denver,co').geocode_data
+
+        expect(search).to be_instance_of(Hash)
+
+        address_components = search[:address_components]
+
+        expect(address_components[0]).to have_key(:long_name)
+        expect(address_components[2]).to have_key(:short_name)
+        expect(address_components[3]).to have_key(:long_name)
+
+        coordinates = search[:geometry][:location]
+
+        expect(coordinates).to have_key(:lat)
+        expect(coordinates).to have_key(:lng)
+      end
+    end
+  end
+end
