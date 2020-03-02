574dbd79901eb755639dba56fb404087584862f2
Scott Regenthal <scottregenthal@gmail.com>
2020-03-01 17:27:06 -0700

    Create facade with data to pass to serializer

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,31 @@
+class ForecastFacade
+  attr_reader :id
+
+  def initialize(location)
+    @id = nil
+    @location = location
+  end
+
+  def geolocation_data
+    @geolocation_data ||= GeocodeService.new(location).geocode_data
+  end
+
+  def forecast_data
+    lat = geolocation_data[:geometry][:location][:lat]
+    long = geolocation_data[:geometry][:location][:lng]
+    @forecast_data ||= DarkskyService.new(lat, long).darksky_data
+  end
+
+  def search_location
+    address_components = geolocation_data[:address_components]
+    PoliticalBoundary.new(address_components).city_state_country
+  end
+
+  def forecast
+    Forecast.new(forecast_data)
+  end
+
+  private
+
+  attr_reader :location
+end
