2dc5b96c7abbf828fe4a5ee8ff23e514b43d3ab6
Scott Regenthal <scottregenthal@gmail.com>
2020-03-02 10:48:08 -0700

    Record VCR cassette

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 describe 'Munchies API endpoint' do
-  it 'can return food & forecast info for a destination city' do
+  it 'can return food & forecast info for a destination city', :vcr do
     get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
 
     expect(response).to be_successful
 
     response_data = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response_data[:data]).to have_key(:munchie)
+    expect(response_data[:data][:attributes]).to have_key(:end_location)
+    expect(response_data[:data][:attributes]).to have_key(:travel_time)
+    expect(response_data[:data][:attributes]).to have_key(:forecast)
+    expect(response_data[:data][:attributes]).to have_key(:restaurant)
 
-    expect(response_data[:attributes]).to have_key(:end_location)
-    expect(response_data[:attributes]).to have_key(:travel_time)
-    expect(response_data[:attributes]).to have_key(:forecast)
-    expect(response_data[:attributes]).to have_key(:restaurant)
-
-    expect(response_data[:attributes][:restaurant]).to have_key(:name)
-    expect(response_data[:attributes][:restaurant]).to have_key(:address)
+    expect(response_data[:data][:attributes][:restaurant]).to have_key(:name)
+    expect(response_data[:data][:attributes][:restaurant]).to have_key(:address)
   end
 end
 
