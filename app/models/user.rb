6c22b92026fddb94a640ed8857798e2c6b219d06
Scott Regenthal <scottregenthal@gmail.com>
2020-03-02 20:18:30 -0700

    Create user model, validating presence of attributes

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password_confirmation, require: true
+
+  has_secure_token :api_key
+  has_secure_password
+end
