af43d748d4b75295bceb0a53c35c30138810b552
Scott Regenthal <scottregenthal@gmail.com>
2020-03-02 20:17:27 -0700

    Silence VCR; remove comments

--- spec/requests/api/v1/registration_request_spec.rb
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 describe 'Registration API endpoint' do
-  it 'successful request creates user and generates unique api key associated with user' do
+  it 'successful request creates user and generates unique api key associated with user', :vcr do
+      WebMock.enable_net_connect!
+      VCR.eject_cassette
+      VCR.turn_off!(ignore_cassettes: true)
+
     headers = { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' }
     params = {
       email: 'whatever@example.com',
@@ -12,17 +16,5 @@ describe 'Registration API endpoint' do
     post '/api/v1/users', params: params
 
     expect(response).to be_successful
-
   end
 end
-
-
-# POST /api/v1/users
-# Content-Type: application/json
-# Accept: application/json
-
-# {
-#   "email": "whatever@example.com",
-#   "password": "password"
-#   "password_confirmation": "password"
-# }
