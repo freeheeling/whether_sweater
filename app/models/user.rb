ee6b0b16fe52ce19c1f9e2b6f9f02ad1935358b5
Scott Regenthal <scottregenthal@gmail.com>
2020-03-03 10:00:31 -0700

    Update user registartion model and test with attribute validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence: true
-  validates_presence_of :password_confirmation, require: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+    presence: true,
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: true
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   has_secure_token :api_key
-  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,11 @@ describe User do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should allow_value('whatever@example.com').for(:email) }
+    it { should_not allow_value('whatever').for(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:password_confirmation) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_length_of(:password_confirmation).is_at_least(6) }
   end
 end
