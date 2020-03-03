21133a56050229cee710cedf753e104826e39c86
Scott Regenthal <scottregenthal@gmail.com>
2020-03-02 21:15:48 -0700

    Render serialized newly created user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    User.create(user_params)
+    user = User.create(user_params)
+    render json: UserSerializer.new(user)
   end
 
   private
