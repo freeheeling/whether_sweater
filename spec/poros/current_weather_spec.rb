23555b30491aa04cd7a448d4365362c5212ab06b
Scott Regenthal <scottregenthal@gmail.com>
2020-03-04 15:10:03 -0700

    Update test for all uv exposure levels associated with uv index number

--- spec/poros/current_weather_spec.rb
+++ spec/poros/current_weather_spec.rb
@@ -3,45 +3,55 @@ require 'rails_helper'
 describe CurrentWeather do
   describe 'instance methods' do
     context '#uv_exposure_level' do
-      xit 'returns associated uv exposure level for uv index number' do
-        current_conditions = {
-          currently: {
-            time: 1583330087,
-            summary: 'Clear',
-            icon: 'clear-night',
-            temperature: 53.28,
-            apparentTemperature: 53.28,
-            humidity: 0.69,
-            visibility: 10,
-            uvIndex: 0
+      it 'returns associated uv exposure level for uv index number' do
+        forecast_data = {
+          :currently => {
+            :time => 1583356555,
+            :summary => 'Clear',
+            :icon => 'clear-day',
+            :precipProbability => 0,
+            :temperature => 66.16,
+            :apparentTemperature => 66.16,
+            :humidity => 0.48,
+            :uvIndex => 5,
+            :visibility => 10,
           },
-          daily: {
-            summary: 'Light rain next Wednesday.'
+          :hourly => {
+            :data => [
+              {
+                  :summary => 'Partly Cloudy',
+                  :icon => 'clear-night'
+              },
+            ]
           },
-          hourly: {
-            data: {
-              icon: 'clear-night',
-              summary: 'Clear'
-            }
+          :daily => {
+            :data => [
+                {
+                  :summary => 'Clear throughout the day.',
+                },
+            ]
           }
         }
 
-        current_weather = CurrentWeather.new(current_conditions)
-
-        expect(current_conditions[:currently][:uvIndex]).to eq(0)
-        expect(current_weather.uv_exposure_level).to eq('low')
-
-        current_conditions[:currently][:uvIndex] = 5
+        current_weather = CurrentWeather.new(forecast_data)
+        expect(forecast_data[:currently][:uvIndex]).to eq(5)
         expect(current_weather.uv_exposure_level).to eq('moderate')
 
-        current_conditions[:currently][:uvIndex] = 7
+        forecast_data[:currently][:uvIndex] = 1
+        current_weather = CurrentWeather.new(forecast_data)
+        expect(current_weather.uv_exposure_level).to eq('low')
+
+        forecast_data[:currently][:uvIndex] = 11
+        current_weather = CurrentWeather.new(forecast_data)
+        expect(current_weather.uv_exposure_level).to eq('extreme')
+
+        forecast_data[:currently][:uvIndex] = 7
+        current_weather = CurrentWeather.new(forecast_data)
         expect(current_weather.uv_exposure_level).to eq('high')
 
-        current_conditions[:currently][:uvIndex] = 10
+        forecast_data[:currently][:uvIndex] = 9
+        current_weather = CurrentWeather.new(forecast_data)
         expect(current_weather.uv_exposure_level).to eq('very high')
-
-        current_conditions[:currently][:uvIndex] = 11
-        expect(current_weather.uv_exposure_level).to eq('extreme')
       end
     end
   end
