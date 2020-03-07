546e4d250be8f1df5433d7c813525228ef121963
Scott Regenthal <scottregenthal@gmail.com>
2020-03-07 06:52:17 -0700

    Add endpoint routed to munchies controller index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#index'
+      get '/forecast', to: 'forecasts#index'
       get '/backgrounds', to: 'backgrounds#index'
+      get '/munchies', to: 'munchies#index'
+
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
       post '/road_trip', to: 'road_trips#create'
